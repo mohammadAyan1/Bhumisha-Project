148d249e6f32dd74d35205d4267e39a8403b8afc
mohammadAyan1 <mohammadayan2210@gmail.com>
2026-01-09 12:37:37 +0530

    use mysql public url for railway

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,8 @@
-require("dotenv").config();
+// require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const db = require("./src/config/db");
 const app = require("./src/app");
 
